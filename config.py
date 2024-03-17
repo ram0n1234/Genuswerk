9ee73f8e30ed495cfbc8fadee23c45ada2671754
ram0n1234 <163744939+ram0n1234@users.noreply.github.com>
2024-03-17 23:56:15 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-sql_user = os.environ.get('sql_user')
-sql_pass = os.environ.get('sql_pass')
-sql_host = os.environ.get('sql_host')
+sql_user = os.environ.get("sql_user")
+sql_pass = os.environ.get("sql_pass")
+sql_host = os.environ.get("sql_host")
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'vNn^D=k8slkjef-f9vjv97r3jfs@#°n'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{sql_user}:{sql_pass}@{sql_host}:3306/genusswerk'
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{sql_user}:{sql_pass}@{sql_host}:3306/genusswerk"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
