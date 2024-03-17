9ee5a90cc8ee9ca0196875ef6497a729298144c9
ram0n1234 <163744939+ram0n1234@users.noreply.github.com>
2024-03-18 00:13:42 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 # Zugriff auf OS-Variablen von Azure App Service
 # Quelle: https://learn.microsoft.com/en-us/azure/app-service/configure-language-python
-sql_user = os.environ['sql_user']
-sql_pass = os.environ['sql_pass']
-sql_host = os.environ['sql_host']
+sql_user = os.environ.get("sql_user")
+sql_pass = os.environ.get("sql_pass")
+sql_host = os.environ.get("sql_host")
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'vNn^D=k8slkjef-f9vjv97r3jfs@#°n'
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{sql_user}:{sql_pass}@{sql_host}:3306/genusswerk"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{sql_user}:{sql_pass}@{sql_host}:3306/genusswerk"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
