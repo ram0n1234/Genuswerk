178285d6b0a3e69455663ec201a106e1b9d77194
ram0n1234 <163744939+ram0n1234@users.noreply.github.com>
2024-03-18 00:05:03 +0100

    Add files via upload
    Azure App Service Env-Variabeln

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 
-sql_user = os.environ.get("sql_user")
-sql_pass = os.environ.get("sql_pass")
-sql_host = os.environ.get("sql_host")
+# Zugriff auf OS-Variablen von Azure App Service
+# Quelle: https://learn.microsoft.com/en-us/azure/app-service/configure-language-python
+sql_user = os.environ['sql_user']
+sql_pass = os.environ['sql_pass']
+sql_host = os.environ['sql_host']
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'vNn^D=k8slkjef-f9vjv97r3jfs@#°n'
